91ef450cd9267cc0284ec6132a3d3657d74346d4
Kevin Minutti <minutti70@gmail.com>
2020-02-19 23:33:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,33 +1,24 @@
-#position_take method for use as helper method
-# def position_taken?(board, index)
-#  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-#   false
-#  else
-#   true
-#  end
-# end
-
-
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-  
-def valid_move?(board, index)
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+def on_board(index_num)
+  if  index_num.between?(0,8) == true
     return true
   else
     return false
   end
+end
+
+def valid_move?(board, index)
+  if (position_take?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
+
+#position_take method for use as helper method
+def position_taken?(board, index)
+ if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  false
+ else
+  true
+ end
 end
