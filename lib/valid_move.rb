9a037bbcc9aa1aafb5c4ff488c47514339fcca0a
Kevin Minutti <minutti70@gmail.com>
2020-02-19 22:48:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,19 +1,15 @@
 
-# use a if method or boolean expression to control return value of method
-#use a helper method- a method within antoher method- to make my code more readable
-
-# def position_taken?(board, index)
-#  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-#   false
-#  else
-#   true
-#  end
-# end
-#
 def valid_move?(board, index)
-end  
+  if position_taken? == true && index.between?(0,8)
+  return true
+else
+  return false
+end
 
-
-#There are two condidtions that need to be met in order for this method to return true
-#1.that the position is on the board
-#2.that the position is not taken.
+def position_taken?(board, index)
+ if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  false
+ else
+  true
+ end
+end
