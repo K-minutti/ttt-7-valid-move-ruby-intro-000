a8794f73ded870c93252f94aca485a2d0a95f737
Kevin Minutti <minutti70@gmail.com>
2020-02-19 23:07:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@
 #  end
 # end
 
-def valid_move?(board, index)
+
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
@@ -25,4 +25,18 @@ def valid_move?(board, index)
     end
   end
 
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
   
+def valid_move?(board, index)
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
