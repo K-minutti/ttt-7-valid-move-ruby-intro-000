9929f2fc6b666e67d25183811441c9800851eb03
Kevin Minutti <minutti70@gmail.com>
2020-02-19 23:10:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,35 +1,35 @@
-#
-# def valid_move?(board, index)
-#   position = index.to_i - 1
-#   if (position_taken?(board, index)) == false && (position.between?(0,8) == true)
-#    return true
-#   else
-#    return false
-#  end
-# end
-#
-# def position_taken?(board, index)
-#  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-#   false
-#  else
-#   true
-#  end
-# end
-
-
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
 
 def valid_move?(board, index)
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
+  position = index.to_i - 1
+  if (position_taken?(board, index)) == false && (position.between?(0,8) == true)
+   return true
   else
-    return false
-  end
+   return false
+ end
 end
+
+def position_taken?(board, index)
+ if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  false
+ else
+  true
+ end
+end
+
+
+#
+#   def on_board?(num)
+#     if num.between?(0, 8) == true
+#       return true
+#     else
+#       return false
+#     end
+#   end
+#
+# def valid_move?(board, index)
+#   if (position_taken?(board, index)) == false && (on_board?(index) == true)
+#     return true
+#   else
+#     return false
+#   end
+# end
